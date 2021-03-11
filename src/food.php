087df041233ce6e191b3f2855d094935df352d1b
Anders <jakeowens@protonmail.com>
2021-03-11 12:05:08 +0100

    Corrected spelling

--- src/food.php
+++ src/food.php
@@ -2,13 +2,13 @@
 <html lang="en">
 <?php
 include "backend/readmd.php";
-$recipies = new ParseRecipies("food");
+$recipes = new ParseRecipes("food");
 ?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="Free and opensource recipe book for the home cook">
+    <meta name="description" content="Free and open-source recipe book for the home cook">
     <link rel="stylesheet" href="css/main.css">
     <title>FOSS food</title>
 </head>
@@ -26,15 +26,15 @@ $recipies = new ParseRecipies("food");
         </form>
         <?php 
             if (isset($_GET["search"])) {
-                $recipies->search(htmlspecialchars($_GET["find"]));
+                $recipes->search(htmlspecialchars($_GET["find"]));
             } else if (isset($_GET["reset"])) {
-                $recipies->search(htmlspecialchars(""));
+                $recipes->search(htmlspecialchars(""));
             }
         ?>
     </div>
     <div class="five">
         <?php 
-            $recipies->fiveRandom();
+            $recipes->fiveRandom();
         ?>
     </div>
     
