21053a60e4ae9788a1fc3a1190a06332ee435737
secretmud <lamefeed@gmail.com>
2021-03-11 11:37:36 +0100

    Added some information and some text to the index

--- src/index.php
+++ src/index.php
@@ -1,5 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+include "backend/readmd.php";
+$drinks = new ParseRecipies("drinks");
+$food = new ParseRecipies("food");
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,11 +13,19 @@
     <link rel="stylesheet" href="css/main.css">
     <title>FOSS food</title>
 </head>
+
 <body>
     <nav class="navigation">
         <a href="index.php">Home</a>
         <a href="drinks.php">Drinks</a>
         <a href="food.php">Food</a>
     </nav>
+    <div class="information">
+        Total amount of recipies: <?php echo ($drinks->total + $food->total) ?><br>
+        This is made up of <?php echo $drinks->total ?> drink(s) and <?php echo $food->total ?> food(s).
+    </div>
+    <div class="support">
+        If you enjoy this project, feel free to support it on <a href="https://github.com/Secretmud/fossfood" target="_blank">GitHub</a>
+    </div>
 </body>
 </html>
